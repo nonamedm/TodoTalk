5e6ba5402d21c91b9f7d549ad587a5ea1e7d08de
leesangbokzzang <70052825+leesangbokzzang@users.noreply.github.com>
2021-04-08 22:47:25 +0900

    [2021-04-08] MYSQL DB 연결완료

--- src/main/java/sjls/todotalk/user/controller/MentorMenteeSchController.java
+++ src/main/java/sjls/todotalk/user/controller/MentorMenteeSchController.java
@@ -1,5 +1,9 @@
 package sjls.todotalk.user.controller;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MentorMenteeSchController {
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@RequestMapping(value="/mentorMenteeSearch", method=RequestMethod.GET)
 	public ModelAndView mentorMenteeSearch(){
 		ModelAndView mav = new ModelAndView();
-		
 		
 		mav.setViewName("mentorMenteeSearch/mentorMenteePage");
 		return mav;
