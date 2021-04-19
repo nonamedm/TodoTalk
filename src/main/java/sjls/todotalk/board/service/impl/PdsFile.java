cc7ce6972cd347af4d8f540732ff35e9b0aca6b6
minsu <minsu@DESKTOP-DDJ82PL>
2021-04-19 14:21:21 +0900

    손민수- 기본이미지 수정

--- src/main/java/sjls/todotalk/board/service/impl/PdsFile.java
+++ src/main/java/sjls/todotalk/board/service/impl/PdsFile.java
@@ -45,7 +45,7 @@ public class PdsFile {
 		List<String>  sfilenames = new ArrayList<String>(); 
 				
 		String         file_name      = null; 
-		String         orgFileName   = null; 
+		String         orgFileName    = null; 
 		String         file_ext       = null; 
 		String         sfile_name     = null; 
 		
@@ -65,19 +65,19 @@ public class PdsFile {
 				
 				// filePath + orgFileName + fileExt 이 존재하면 
 				//  중복되지 않는 새로운 파일명을 생성
-				sfile_name = checkFile.getCheckFileName(
-						filePath, orgFileName, file_ext );
+//				sfile_name = checkFile.getCheckFileName(
+//						filePath, orgFileName, file_ext );
 				
 				filenames.add( file_name );
 				fileexts.add( file_ext );
-				sfilenames.add( sfile_name );
+//				sfilenames.add( sfile_name );
 								
 				map.put("file_name",  file_name);
 				map.put("file_ext",   file_ext);
 				map.put("sfile_name", sfile_name);
 				
 				// 파일 저장 : c:\\upload\\
-				File file = new File(filePath + sfile_name);				
+				File file = new File(filePath + filenames);				
 				try {
 					multipartFile.transferTo(file);   // 실제파일 저장
 				} catch( IllegalStateException e ) {
